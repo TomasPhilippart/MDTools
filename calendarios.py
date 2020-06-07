0d8a58aaa403e9ec02020e4494e5fbf75d4557d2
TomasPhilippart <tomasphilippart@gmail.com>
2020-06-07 17:18:18 +0100

    + Pascoa Gregoriana
    by gugz

--- calendarios.py
+++ calendarios.py
@@ -1,4 +1,5 @@
 import math
+from func_aux import *
 
 """
 .----------------.-------.--------.
@@ -129,39 +130,50 @@ def pascoa_juliana(Y):
 
 def pascoa_gregoriana(Y):
     
+    print(f"{bcolors.FAIL}-= Regressões (I) =-{bcolors.ENDC}")
+    I = Y - 1 + Y // 4 - Y//100 + Y//400
+    print(f"I = {Y} - 1 + {Y//4} - {Y//100} + {Y//400} = {I}")
+    print(f"{bcolors.FAIL}-= Nº dominical do ano (N) =-{bcolors.ENDC}")
+    N = 7 - (I % 7)
+    print(f"N = 7 - ({I} % 7) = {N}")
+
+    print(f"{bcolors.FAIL}-= Nº de ouro (G) =-{bcolors.ENDC}")
     G = 1 + Y%19
-    print(f"G = {G}")
+    print(f"G = 1 + {Y} % 19 = {G}")
 
-
+    print(f"{bcolors.FAIL}-= Epact (E) = E0 + V =-{bcolors.ENDC}")
+    print(f"{bcolors.BOLD}EO (formula): [ 57 + 11G - S + fl(S/4) + fl((S - fl((S-17)/25))/3) ] % 30{bcolors.ENDC}")
     Mod1 = 57 + 11 * G
     Mod2 = math.floor(Y/100)
     Mod3 = math.floor(Mod2 / 4)
     ModAux = Mod2 - math.floor((Mod2 - 17) / 25)
     Mod4 = math.floor(ModAux / 3)
 
+
     E = (Mod1 - Mod2 + Mod3 + Mod4) % 30
-    print(f"E0 = {E}")
+    print(f"E0 = ({Mod1} - {Mod2} + {Mod3} + {Mod4}) % 30 = {E}")
     
     V = (E//24 - E//25) + G//12 * (E//25 - E//26)
-    print(f"V = {V}")
+    print(f"V = ({E}//24 - {E}//25) + {G}//12 * ({E}//25 - {E}//26) = {V}")
     E += V
-    print(f"E = {E}")
+    print(f"E = E0 + V = {E}")
 
+    print(f"{bcolors.FAIL}-= Data da Lua Cheia (D) =-{bcolors.ENDC}")
     D = 20 + (54 - E)%30
-    print(f"D = {D}")
-    C = (D + 2)%7 + 1
-    print(f"C = {C}")
-    N = 7 - ((Y - 1 + Y // 4 - Y//100 + Y//400) % 7)
-    print(f"N = {N}")
+    print(f"D = 20 + (54 - {E}) % 30 = {D}")
 
+    print(f"{bcolors.FAIL}-= Nº Calêndrico da Data (C) =-{bcolors.ENDC}")
+    C = (D + 2)%7 + 1
+    print(f"C = ({D} + 2) % 7 + 1 = {C}")
+
+    print(f"{bcolors.FAIL}-= Domingo de Páscoa (S) =-{bcolors.ENDC}")
     if C < N:
         S = D + N - C
-        print(f"S = {S}")
+        print(f"S = {D} + {N} - {C} = {S}")
     else:
         S = D + 7 - (C - N)%7
-        print(f"S = {S}")
+        print(f"S = {D} + 7 - ({C} - {N}) % 7 = {S}")
     
     return S
-
 
 
