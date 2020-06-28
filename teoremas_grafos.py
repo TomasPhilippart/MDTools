01f6c765cc01f57b17a848126308b1e8fd0319aa
TomasPhilippart <tomasphilippart@gmail.com>
2020-06-28 10:35:47 +0100

    Update teoremas_grafos.py

--- teoremas_grafos.py
+++ teoremas_grafos.py
@@ -8,14 +8,14 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-<<<<<<< HEAD
+
 def teoremas_grafos():
     print(f"{bcolors.FAIL}//// TEOREMAS SOBRE GRAFOS ////\n\n{bcolors.ENDC}")
 
     #Primeiro teorema
     print(f"{bcolors.OKBLUE}-= Primeiro teorema da teoria dos grafos =-{bcolors.ENDC}")
     print("A soma dos graus de um grafo de p vertices e igual a 2 * q (numero de arestas)")
-=======
+
 def nocoes_grafos():
     print(f"\n{bcolors.FAIL}//// NOCOES BASICAS SOBRE GRAFOS ////\n{bcolors.ENDC}")
     
@@ -45,38 +45,37 @@ def teoremas_grafos():
     #Primeiro teorema
     print(f"{bcolors.OKBLUE}-= Primeiro teorema da teoria dos grafos =-{bcolors.ENDC}")
     print("A soma dos graus de um grafo de p vertices e igual a 2 * q (numero de arestas).\n")
->>>>>>> MDTools-Funky
+
     print(f"{bcolors.OKGREEN}(u,v) e (v,u) correspondem a mesma aresta, logo a mesma aresta e contabilizada 2 vezes.\n{bcolors.ENDC}")
 
     #Numero par de vertices impares
     print(f"{bcolors.OKBLUE}-= Segundo teorema da teoria dos grafos =-{bcolors.ENDC}")
-<<<<<<< HEAD
+
     print("Um grafo possui sempre um numero par de vertices impares")
-=======
+
     print("Um grafo possui sempre um numero par de vertices impares.\n")
->>>>>>> MDTools-Funky
+
     print(f"{bcolors.OKGREEN}Se a soma dos graus dos vertices e par, e os outros vertices tem grau par (logo a soma e par),\na soma dos vertices de grau impar tem de ser par, e, portanto, tem de existir um numero par de vertices impares.\n{bcolors.ENDC}")
 
     #Limite de arestas
     print(f"{bcolors.OKBLUE}-= Teorema (limites do numero de arestas) =-{bcolors.ENDC}")
-<<<<<<< HEAD
+
     print("Um grafo de p vertices e k componentes possui um numero de arestas (q) tal que:\np - k <= q <= (p-k+1)(p-k)/2.")
     print(f"{bcolors.OKGREEN}O limite minimo e para k arvores, e o limite maximo e para\numa componente de p-k vertices e k componentes de 1 vertice.\n{bcolors.ENDC}")
 
     ##Corolarios
     print(f"{bcolors.OKBLUE}-= Corolarios =-{bcolors.ENDC}")
-=======
+
     print("Um grafo de p vertices e k componentes possui um numero de arestas (q) tal que:\np - k <= q <= (p-k+1)(p-k)/2.\n")
     print(f"{bcolors.OKGREEN}O limite minimo e para k arvores, e o limite maximo e para\numa componente de p-k vertices e k componentes de 1 vertice.\n{bcolors.ENDC}")
 
     ##Corolarios
     print(f"{bcolors.OKGREEN}-= Corolarios =-{bcolors.ENDC}")
->>>>>>> MDTools-Funky
+
     print("Se o grafo possui mais de (p-1)(p-2)/2 componentes, e conexo (possui uma componente)\nUsa a formula para k = 2 e confirma!\n")
     print("Uma arvore possui o minimo de arestas possivel: p-1 - Usa a formula para k = 1 e confirma!\n")
 
     #Teoremas de Euler-Hierholzer
-<<<<<<< HEAD
     print(f"{bcolors.OKBLUE}-= Teoremas de Euler-Hierholzer =-{bcolors.ENDC}")
     print("Um grafo tem circuito euleriano se e so se for conexo e todos os seus vertices forem pares.\n")
     print("Um grafo e atravessavel se e so se for conexo e tiver apenas 2 vertices impares\n(que, ao serem ligados temporariamente, formam um circuito euleriano).\n")
@@ -102,7 +101,7 @@ def nocoes_grafos():
 
     #Pseudografo
     print("Um pseudografo e um grafo que pode contar multiplas arestas entre vertices\ne lacos (arestas de um vertice para ele proprio).\n\n")
-=======
+
     print(f"{bcolors.OKBLUE}-= Teoremas de Euler-Hierholzer (Suporte ao algoritmo de Fleury) =-{bcolors.ENDC}")
     print("Um grafo tem circuito euleriano se e so se for conexo e todos os seus vertices forem pares.\n")
     print("Um grafo e atravessavel se e so se for conexo e tiver apenas 2 vertices impares\n(que, ao serem ligados temporariamente, formam um circuito euleriano).\n")
@@ -139,4 +138,3 @@ def nocoes_grafos():
     print(f"{bcolors.OKBLUE}-= Teorema de Kuratowski =-{bcolors.ENDC}")
     print("Um grafo e planar se e so se nao possuir\n(se nao for homeomorfico com) um subgrafo nao planar.\n")
     print(f"{bcolors.OKGREEN}Sendo K5 e K3,3 nao planares, se encontrarmos um subgrafo equivalente\na um desses dois, podemos concluir que o grafo em causa nao e planar.{bcolors.ENDC}\n")
->>>>>>> MDTools-Funky
