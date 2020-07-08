3b0ed868290a1f08fafaa7d6d3c6036b63710c11
FunkyCracky <futebol4@hotmail.com>
2020-07-08 18:08:49 +0100

    Melhorias no algoritmo de Fleury (repetição da pergunta em caso de input incorreto)

--- percorrer_grafos.py
+++ percorrer_grafos.py
@@ -10,29 +10,30 @@ class bcolors:
 
 def fleury_apl():
     print(f"{bcolors.FAIL}-= Algoritmo de Fleury =-{bcolors.ENDC}")
-    Conexo = input("O grafo e conexo? [Y/N] ")
-    if Conexo.upper() == 'Y':
-        Euleriano = input("Todos os vertices do grafo tem grau par? [Y/N] ")
-        if Euleriano.upper() == 'Y':
-            print(f"{bcolors.OKGREEN}O grafo e euleriano!{bcolors.ENDC}")
-            print(f"{bcolors.OKGREEN}Algoritmo de Fleury aplicavel, comecando em qualquer vertice e acabando nele proprio.{bcolors.ENDC}")
-            return True
-        elif Euleriano.upper() == 'N':
-            print(f"{bcolors.WARNING}O grafo nao e euleriano.{bcolors.ENDC}")
-            Atravessavel = input("O grafo possui apenas 2 vertices de grau impar? [Y/N] ")
-            if Atravessavel.upper() == 'Y':
-                print(f"{bcolors.OKGREEN}O grafo e atravessavel!{bcolors.ENDC}")
-                print(f"{bcolors.OKGREEN}Algoritmo de Fleury aplicavel, comecando num dos vertices de grau impar e acabando no outro.{bcolors.ENDC}")
+    while True:
+        Conexo = input("O grafo e conexo? [Y/N] ")
+        if Conexo.upper() == 'Y':
+            Euleriano = input("Todos os vertices do grafo tem grau par? [Y/N] ")
+            if Euleriano.upper() == 'Y':
+                print(f"{bcolors.OKGREEN}O grafo e euleriano!{bcolors.ENDC}")
+                print(f"{bcolors.OKGREEN}Algoritmo de Fleury aplicavel, comecando em qualquer vertice e acabando nele proprio.{bcolors.ENDC}")
                 return True
+            elif Euleriano.upper() == 'N':
+                print(f"{bcolors.WARNING}O grafo nao e euleriano.{bcolors.ENDC}")
+                Atravessavel = input("O grafo possui apenas 2 vertices de grau impar? [Y/N] ")
+                if Atravessavel.upper() == 'Y':
+                    print(f"{bcolors.OKGREEN}O grafo e atravessavel!{bcolors.ENDC}")
+                    print(f"{bcolors.OKGREEN}Algoritmo de Fleury aplicavel, comecando num dos vertices de grau impar e acabando no outro.{bcolors.ENDC}")
+                    return True
+                elif Atravessavel.upper() == 'N':
+                    print(f"{bcolors.FAIL}Algoritmo de Fleury nao aplicavel.{bcolors.ENDC}")
+                    return False
+                else:
+                    print(f"{bcolors.WARNING}Input incorreto, tenta de novo!{bcolors.ENDC}")
             else:
-                print(f"{bcolors.FAIL}Algoritmo de Fleury nao aplicavel.{bcolors.ENDC}")
-                return False
+                print(f"{bcolors.WARNING}Input incorreto, tenta de novo!{bcolors.ENDC}")
+        elif Conexo.upper() == 'N':
+            print(f"{bcolors.FAIL}Algoritmo de Fleury nao aplicavel.{bcolors.ENDC}")
+            return False
         else:
             print(f"{bcolors.WARNING}Input incorreto, tenta de novo!{bcolors.ENDC}")
-            return False
-    elif Conexo.upper() == 'N':
-        print(f"{bcolors.FAIL}Algoritmo de Fleury nao aplicavel.{bcolors.ENDC}")
-        return False
-    else:
-        print(f"{bcolors.WARNING}Input incorreto, tenta de novo!{bcolors.ENDC}")
-        return False
